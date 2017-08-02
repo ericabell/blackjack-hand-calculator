cbb3b6000e3f24ac57c89917b8f78c22023878e5
Eric Abell <ericabell@users.noreply.github.com>
2017-08-02 13:50:44 -0500

    Cleaned up naming of variables and re-factored a bit

--- scripts/main.js
+++ scripts/main.js
@@ -12,35 +12,35 @@
 
 function handValue (hand) {
   // hand is a list of strings with values "2", "3", etc.
-  let total = 0; // this will hold our hand total value
+  let totalInHand = 0; // this will hold our hand total value
   let ace_total = 0;  // keep track of the number of aces, and determine
                       // their value at the END OF THE HAND :-(
   hand.forEach(function(card){
-    let value = 0; // reset the value of each card to 0
+    let valueOfCard = 0; // reset the valueOfCard of each card to 0
     if(!parseInt(card)) { // check if we have a face card or a number card
       // face card
       // J, Q, K,  or A
       if(card==='A') {
         ace_total +=1;
       } else {
-        value = 10;  // otherwise, it must be J, Q, K
+        valueOfCard = 10;  // otherwise, it must be J, Q, K
       }
     } else {
-      value = parseInt(card); // a number card just has it's value
+      valueOfCard = parseInt(card); // a number card just has it's valueOfCard
     }
-    total += value; // add the card to our running total
+    totalInHand += valueOfCard; // add the card to our running total
   });
 
   // we have processed all the cards, now determine what As are worth
   if(ace_total>0) {
-    if(total>21 || total>10){
+    if(totalInHand>21 || totalInHand>10){
       // all the aces are 1s
-      total += ace_total;
+      totalInHand += ace_total;
     } else {
-      total += ace_total*11;
+      totalInHand += ace_total*11;
     }
   }
-  return total;
+  return totalInHand;
 }
 
 
