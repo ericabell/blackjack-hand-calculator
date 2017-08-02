895f09f5e4a5edfe86e344cd1acf9ea0cefc6fc6
Eric Abell <ericabell@users.noreply.github.com>
2017-08-02 13:34:16 -0500

    Works

--- scripts/main.js
+++ scripts/main.js
@@ -5,15 +5,46 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
 function handValue (hand) {
-
-
-  return;
+  // hand is a list of strings with values "2", "3", etc.
+  console.log('handValue');
+  console.log(hand);
+  let total = 0;
+  let ace_total = 0;
+  hand.forEach(function(card){
+    console.log(card);
+    let value = 0;
+    if(!parseInt(card)) {
+      console.log('face card detected');
+      // face card
+      // J, Q, K,  or A
+      if(card==='A') {
+        console.log('ace detected');
+        ace_total +=1;
+      } else {
+        value = 10;
+      }
+    } else {
+      value = parseInt(card);
+    }
+    console.log(value);
+    total += value;
+  });
+  if(ace_total>0) {
+    if(total>21 || total>10){
+      // all the aces are 1s
+      total += ace_total;
+    } else {
+      total += ace_total*11;
+    }
+  }
+  console.log(total);
+  return total;
 }
 
 
